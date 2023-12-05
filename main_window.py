922d2916a37ea6d0cc568555428731a53fa7cdde
Luiz Cheva <luiz-cheva@hotmail.com>
2023-12-05 11:11:30 -0300

    Create a layout for main window

--- main_window.py
+++ main_window.py
@@ -1,4 +1,9 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout
+from PySide6.QtWidgets import (
+    QMainWindow, QFrame, QHBoxLayout, QVBoxLayout
+)
+from widgets.pages.ui_main import (
+    LeftMenu, ContentObject, StatusBar, ContentPage
+)
 
 
 class MainWindows(QMainWindow):
@@ -9,8 +14,36 @@ class MainWindows(QMainWindow):
         self.resize(1200, 720)
         self.setMinimumSize(960, 540)
 
-        self.cw = QWidget()
-        self.main_window = QHBoxLayout(self.cw)
+        self.cf = QFrame()
+        self.main_window = QHBoxLayout(self.cf)
         self.main_window.setContentsMargins(0, 0, 0, 0)
         self.main_window.setSpacing(0)
-        self.setCentralWidget(self.cw)
+
+        # LEFT MENU
+        self.left_menu = LeftMenu()
+
+        # CONTENT
+        self.content = ContentObject()
+
+        # ADD TO LAYOUT
+        self.main_window.addWidget(self.left_menu)
+        self.main_window.addWidget(self.content)
+
+        # LAYOUT FOR CONTENT
+        self.content_layout = QVBoxLayout(self.content)
+        self.content_layout.setContentsMargins(0, 0, 0, 0)
+        self.content_layout.setSpacing(0)
+
+        # STATUS BAR
+        self.top_stuatus_bar = StatusBar()
+        self.bottom_status_bar = StatusBar()
+
+        # CONTENT PAGE
+        self.content_page = ContentPage()
+
+        # ADD TO LAYOUT CONTENT
+        self.content_layout.addWidget(self.top_stuatus_bar)
+        self.content_layout.addWidget(self.content_page)
+        self.content_layout.addWidget(self.bottom_status_bar)
+
+        self.setCentralWidget(self.cf)
---
+++ widgets/pages/ui_main.py
@@ -0,0 +1,30 @@
+from PySide6.QtWidgets import QFrame
+
+
+class LeftMenu(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setStyleSheet('background-color: #853276')
+        self.setMaximumWidth(50)
+        self.setMinimumWidth(50)
+
+
+class ContentObject(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setStyleSheet('background-color: white')
+
+
+class StatusBar(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setStyleSheet('background-color: #f2d0d9')
+        self.setMaximumHeight(30)
+        self.setMinimumHeight(30)
+
+
+class ContentPage(QFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
